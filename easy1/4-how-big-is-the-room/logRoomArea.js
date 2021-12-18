5eb193196bce8c2641668b5e8f52aa0d6ca7f00a
Joshua Ordehi <mev.dev.kar@gmail.com>
2021-12-18 00:08:15 -0400

    finish further exploration for exercise 4

--- easy1/4-how-big-is-the-room/logRoomArea.js
+++ easy1/4-how-big-is-the-room/logRoomArea.js
@@ -17,7 +17,7 @@ const logRoomArea = () => {
   );
 };
 
-logRoomArea();
+// logRoomArea();
 
 /* 
 Further Exploration
@@ -25,15 +25,29 @@ Modify the program so that it asks the user for the input type (meters or feet).
 */
 
 const logRoomAreaWithType = () => {
-  let inputType = 0;
-  let roomLength = rlSync.question('Enter the length of the room in meters:\n');
-  let roomWidth = rlSync.question('Enter the width of the room in meters:\n');
-  let areaSqMeters = parseFloat(roomLength * roomWidth).toFixed(2);
-  let areaSqFeet = (areaSqMeters * 10.7639).toFixed(2);
-
-  console.log(
-    `The area of the room is ${areaSqMeters} square meters (${areaSqFeet} square feet).`
+  let inputType = rlSync.question(
+    'Will you input meters or feet?\n1. meters\n2. feet\n'
   );
+
+  let roomLength = rlSync.question('Enter the length of the room:\n');
+  let roomWidth = rlSync.question('Enter the width of the room:\n');
+
+  let areaSqMeters;
+  let areaSqFeet;
+
+  if (inputType === '1') {
+    areaSqMeters = (roomLength * roomWidth).toFixed(2);
+    areaSqFeet = (areaSqMeters * 10.7639).toFixed(2);
+    console.log(
+      `The area of the room is ${areaSqMeters} square meters (${areaSqFeet} square feet).`
+    );
+  } else if (inputType === '2') {
+    areaSqFeet = (roomLength * roomWidth).toFixed(2);
+    areaSqMeters = (areaSqFeet / 10.7639).toFixed(2);
+    console.log(
+      `The area of the room is ${areaSqFeet} square feet (${areaSqMeters} square meters).`
+    );
+  }
 };
 
 logRoomAreaWithType();
